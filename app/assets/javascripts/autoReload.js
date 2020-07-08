0ed32e7e928386c217f7cedc38f2795faa57124f
takumi-19 <tkm19.k.univ@icloud.com>
2020-07-08 12:19:24 +0900

    Add automatic update function

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if (message.image) {
       let html = 
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo">
             <div class="MessageInfo__userName">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo">
             <div class="MessageInfo__userName">
               ${message.user_name}
@@ -40,27 +40,27 @@ $(function(){
     };
   }
 
-  $('.MessageForm__formWith').on('submit', function(e){
-    e.preventDefault()
-    let formData = new FormData(this);
-    let url = $(this).attr('action')
+  let reloadMessages = function() {
+    let last_message_id = $(".MessageBox:last").data("message-id");
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,  
-      dataType: 'json',
-      processData: false,
-      contentType: false
+      url: "api/messages",
+      type: "GET",
+      dataType: "json",
+      data: {id: last_message_id}
     })
-    .done(function(data){
-      let html = buildHTML(data);
-      $('.MessageList').append(html);
-      $('.MessageList').animate({ scrollTop: $('.MessageList')[0].scrollHeight});
-      $('form')[0].reset();
-      $('.SendBtn').prop('disabled', false);
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        let insertHTML = "";
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $(".MessageList").append(insertHTML);
+        $('.MessageList').animate({ scrollTop: $('.MessageList')[0].scrollHeight});
+      }
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
-    })
-  })
-})
+      alert("error");
+    });
+  };
+  setInterval(reloadMessages, 7000);
+});
---
+++ app/assets/javascripts/modules/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image) {
+      let html = 
+        `<div class="MessageBox" data-message-id=${message.id}>
+          <div class="MessageInfo">
+            <div class="MessageInfo__userName">
+              ${message.user_name}
+            </div>
+            <div class="MessageInfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+        `<div class="MessageBox" data-message-id=${message.id}>
+          <div class="MessageInfo">
+            <div class="MessageInfo__userName">
+              ${message.user_name}
+            </div>
+            <div class="MessageInfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+
+  $(".MessageForm__formWith").on("submit", function(e){
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr("action")
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,  
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      let html = buildHTML(message);
+      $(".MessageList").append(html);
+      $(".MessageList").animate({ scrollTop: $(".MessageList")[0].scrollHeight});
+      $("form")[0].reset();
+      $(".SendBtn").prop("disabled", false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  });
+});
